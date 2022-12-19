0a49df137cf53d451dce1c30ff9d7b51c2cc5af4
harishboha <boharah2059@gmail.com>
2022-12-19 12:54:49 +0545

    ptr/100

--- HelloHarish/EntryPoint.cs
+++ HelloHarish/EntryPoint.cs
@@ -1,24 +1,41 @@
 
 
-class EntryPoint
-{
-    public static void Main()
-    {  
-        var input = Console.ReadLine();        
-        var number = 65;
+// class EntryPoint
+// {
+//     public static void Main()
+//     {  
+//         var input = Console.ReadLine();        
+//         var number = 65;
 
-        if(number % 2 == 0)
-        {
-            Console.WriteLine(number + " is even");
-        }
-        else
-        {
-            Console.WriteLine(number + " is odd");
-        }
+//         if(number % 2 == 0)
+//         {
+//             Console.WriteLine(number + " is even");
+//         }
+//         else
+//         {
+//             Console.WriteLine(number + " is odd");
+//         }
 
         
-        CW - Calculate simple interest for pricipal amount, time and rate given by user.
+//         CW - Calculate simple interest for pricipal amount, time and rate given by user.
 
-        Console.ReadKey();
-    }
+//         Console.ReadKey();
+//     }
+// }
+
+class classwork
+{
+    public static void Main()
+   {
+    Console.WriteLine(" enter an amount");
+    int amt = int.parse(console.ReadLine());
+    console.writeline("enter a time period:");
+    int ti = int.parse(console.ReadLine());
+    console.writeline("enter a interest");
+    int rate = int.parse(console.ReadLine());
+    int si = amt*ti*rate/100;
+    console.writeline(" the simple interest is ="+si);
+   }
+
+
 }
